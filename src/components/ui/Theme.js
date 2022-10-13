a0e014fefcafe13675b7af76a699c53f4ec64899
MalachiCunliffe <mal.cunliffe@gmail.com>
2022-10-13 21:15:54 +1300

    start theme build out

--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -1,5 +1,16 @@
 import { createMuiTheme } from "@material-ui/core";
 
-export default createMuiTheme({
+const arcBlue = "#0B72B9";
+const arcOrange = "#FFBA60";
 
+export default createMuiTheme({
+    palette:{
+        common:{
+            arcBlue: `${arcBlue}`,
+            arcOrange: `${arcOrange}`
+        },
+        primary:{
+            main: `${arcBlue}`
+        }
+    }
 })
